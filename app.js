8b0e7bbb710b82639d21d2aa612be12366f0692c
Paulo Henrique <83429007+paulohldev@users.noreply.github.com>
2022-12-02 23:16:51 -0300

    primeiro commit

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const mysql = require('mysql2');
+const connect = require('./conexao.js');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.put('/clientes/:id', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  return connect.execSQLQuery(
+    "update cliente set nome='" + req.body.nome + "' where id=" + req.params.id,
+    res,
+  );
+});
+
+app.post('/clientes/', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  return connect.execSQLQuery(
+    "insert into cliente (nome) value('" + req.body.nome + "')",
+    res,
+  );
+});
+
+app.delete('/clientes/:id', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  return connect.execSQLQuery(
+    'delete from cliente where id=' + req.params.id,
+    res,
+  );
+});
+
+app.get('/', (req, res) => {
+  res.send('Testando App');
+  res.end();
+});
+
+app.get('/clientes', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  return connect.execSQLQuery('select * from cliente', res);
+});
+
+app.get('/clientes/:id', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
+  return connect.execSQLQuery(
+    'select * from cliente where id=' + req.params.id,
+    res,
+  );
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server iniciado na porta ${PORT}`));
